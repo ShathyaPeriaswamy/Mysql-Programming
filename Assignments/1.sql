fba48ae7d3088cfd6c3d3e76e271a7de60c15c56
ShathyaPeriaswamy <37336048+ShathyaPeriaswamy@users.noreply.github.com>
2018-04-30 11:36:05 +0530

    Create 1.sql

---
+++ Assignments/1.sql
@@ -0,0 +1,16 @@
+create database students_record;
+create table informations
+(
+First_name char(20) NOT NULL,
+Last_name char(20) NOT NULL,
+Roll_no varchar(15) NOT NULL,
+Department char(20) NOT NULL,
+Place char(15) NOT NULL,
+School char(20) NOT NULL,
+10th_marks int(10) NULL,
+12th_marks int(10) NOT NULL,
+Age int(5) NOT NULL,
+Siblings_name char(20) NOT NULL
+);
+describe informations
+
